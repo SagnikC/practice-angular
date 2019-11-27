8b0370c8c27e3db8466550406ff167dcfc11b762
Sagnik Chatterjee <sagnik.chatterjee17@gmail.com>
2019-11-27 09:52:10 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Added by me
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //added by me
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
+  //added by me
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//router module used for setting up the application level route
+//import {RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
+
+    //routerModule forRoot method to declare the possible routes in the application
+    /*RouterModule.forRoot([
+      {path: 'home', component: HomeComponent}
+    ]),*/
+
     AppRoutingModule
   ],
   providers: [],
